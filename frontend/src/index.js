37b265c5f1d493ecd5c710c4c65ae443f825162a
Chamika Dilshan <147516486+TechDilshan@users.noreply.github.com>
2024-09-19 12:58:13 +0530

    index change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
+//import Feedback from './Client/feedback';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<App />} />
+        {/* <Route path='/feedback' element={<Users />} /> */}
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
