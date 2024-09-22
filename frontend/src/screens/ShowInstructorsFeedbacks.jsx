78bf9ef05f659d5fdc7821757452a625c992ad8f
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-09-22 13:03:57 +0530

    package reviews

--- frontend/src/screens/ShowInstructorsFeedbacks.jsx
+++ frontend/src/screens/ShowInstructorsFeedbacks.jsx
@@ -4,7 +4,7 @@ import InstructorFeedback from '../components/InstructorFeedback'
 
 const ShowInstructorsFeedbacks = () => {
   return (
-    <div className='flex w-screen min-h-screen bg-cover bg-center bg-no-repeat justify-center items-center' style={{ backgroundImage: 'url(/images/sven-mieke-jO6vBWX9h9Y-unsplash.jpg)' }}>
+    <div className='flex w-full min-h-screen bg-cover bg-center bg-fixed bg-no-repeat justify-center items-center' style={{ backgroundImage: 'url(/images/sven-mieke-jO6vBWX9h9Y-unsplash.jpg)' }}>
       <div className='flex justify-center flex-col gap-6 p-10 bg-[#c7c7c72c] rounded-2xl backdrop-blur-sm items-center'>
         <SearchBar placeholder='Search for your Instructor ..' />
         <div className='flex justify-center flex-col gap-3'>
--- frontend/src/screens/ShowPackagesFeedbacks.jsx
+++ frontend/src/screens/ShowPackagesFeedbacks.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import SearchBar from '../components/SearchBar'
+import PackageFeedback from '../components/PackageFeedback'
 
 const ShowPackagesFeedbacks = () => {
   return (
-    <div>ShowPackagesFeedbacks</div>
+    <div className='flex w-full min-h-screen bg-fixed bg-cover bg-center bg-no-repeat justify-center items-center' style={{ backgroundImage: 'url(/images/sven-mieke-jO6vBWX9h9Y-unsplash.jpg)' }}>
+      <div className='flex justify-center flex-col gap-6 p-10 bg-[#c7c7c72c] rounded-2xl backdrop-blur-sm items-center'>
+        <SearchBar placeholder='Search for your Instructor ..' />
+        <div className='flex justify-center flex-col gap-3'>
+          <PackageFeedback gymPackage='silver' />
+          <PackageFeedback gymPackage='gold' />
+          <PackageFeedback gymPackage='platinum' />
+        </div>
+        </div>
+    </div>
   )
 }
 
